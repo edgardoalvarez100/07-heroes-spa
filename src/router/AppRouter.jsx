7e111566cd58a00ae1e9c087d6fd435aa96abce0
Edgardo Alvarez <edgardo@livedesign.org>
2023-08-25 17:24:05 -0500

    “update”

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Routes, Navigate, Route } from 'react-router-dom'
+import { MarvelPage, DcPage } from '../heroes'
+
+import { LoginPage } from '../auth'
+import { Navbar } from '../ui'
+
+export const AppRouter = () => {
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="marvel" element={<MarvelPage />} />
+                <Route path="dc" element={<DcPage />} />
+                <Route path="login" element={<LoginPage />} />
+
+                <Route path="*" element={<Navigate to="/marvel" />} />
+            </Routes>
+        </>
+    )
+}
