728485aaa9775937b3315f86dc8153c2f09d8492
Edgardo Alvarez <edgardo@livedesign.org>
2023-08-26 07:34:29 -0500

    “update”

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,21 +1,44 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
+import { useMemo } from "react";
 
 
 export const HeroPage = () => {
 
     const { id } = useParams();
-    const hero = getHeroById(id)
+    const navegate = useNavigate()
+    const hero = useMemo(() =>
+        getHeroById(id)
+        , [id])
+
+    const onNavigateBack = () => {
+        navegate(-1);
+    }
 
     if (!hero) {
         return <Navigate to="/" />
     }
-
-
-
     return (
         <>
-            {JSON.stringify(hero)}
+            <div className="row mt-5 animate__animated animate__fadeIn">
+                <div className="col-4 ">
+                    <img src={`/assets/${hero.id}.jpg`} alt={hero.superhero}
+                        className="img-thumbnail animate__animated animate__fadeInLeft" />
+                </div>
+                <div className="col-8">
+                    <h3>{hero.superhero}</h3>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego}</li>
+                        <li className="list-group-item"><b>Publisher:</b> {hero.publisher}</li>
+                        <li className="list-group-item"><b>First appereance:</b> {hero.first_appearance}</li>
+                    </ul>
+
+                    <h5 className="mt-3">Characters</h5>
+                    <p>{hero.characters}</p>
+
+                    <button className="btn btn-outline-primary" onClick={onNavigateBack}>Back</button>
+                </div>
+            </div>
         </>
     )
 }
